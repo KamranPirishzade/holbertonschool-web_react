07ea3cd4d2ec5b4e49834b291b5bc18fcd231ee6
Kamran <KamranPirishzade@gmail.com>
2025-07-13 04:52:34 -0700

    Webpack task 3 fix

--- Webpack/task_3/modules/body/body.js
+++ Webpack/task_3/modules/body/body.js
@@ -1,4 +1,15 @@
-import './body.css';
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from "jquery";
+import lodash from "lodash";
+import "./body.css";
 
+const $button = $("<button>Click here to get started</button>");
+$("body").append($button);
+$("body").append('<p id="count"></p>');
+
+let count = 0;
+const updateCounter = () => {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+};
+
+$button.on("click", lodash.debounce(updateCounter, 500));
--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,46 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-	entry: {
-		all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
-	  },  output: {
-    path: path.resolve(__dirname, 'public'),
-	filename: '[name].bundle.js',
-},
-plugins: [
-	new CleanWebpackPlugin(),
-	new HtmlWebpackPlugin({
-	title: 'Holberton Dashboard',
-    }),
-	],
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './public',
-		port: 8564,
-	},
-
-  mode: 'development',
+  mode: "development",
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public/"),
+  },
+  devtool: "inline-source-map",
+  plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
+  devServer: {
+    port: 8564,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(gif|png|jpe?g|svg)$/i,
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
-	],
-},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/i,
+        use: ["file-loader", "image-webpack-loader"],
+        type: "asset/resource",
+      },
+    ],
+  },
 };
