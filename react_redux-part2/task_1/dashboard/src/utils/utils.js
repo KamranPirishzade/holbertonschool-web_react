382048db522ec65d55b947be2113d0954fa998f2
Kamran <KamranPirishzade@gmail.com>
2025-08-10 10:30:08 -0700

    Redux part 2

--- react_redux-part2/task_1/dashboard/src/utils/utils.js
+++ react_redux-part2/task_1/dashboard/src/utils/utils.js
@@ -1,14 +1,5 @@
-import React from 'react';
+export const getCurrentYear = () => new Date().getFullYear();
 
-export const getFullYear = () => {
-  return new Date().getFullYear();
-};
+export const getFooterCopy = (isIndex) => isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 
-export const getFooterCopy = (isIndex) => {
-  if (isIndex) return 'Holberton School';
-  return 'Holberton School main dashboard';
-};
-
-export const getLatestNotification = () => {
-  return '<strong>Urgent requirement</strong> - complete by EOD';
-};
+export const getLatestNotification = () => '<strong>Urgent requirement</strong> - complete by EOD'
